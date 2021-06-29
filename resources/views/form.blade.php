44ad5a527ead88ad7dbd5597387e1f2a37021657
Fernando-Linhares <fernandomadethat@gmail>
2021-06-29 18:52:24 -0300

    add blade code

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div  class="card card-sm card-border">
+            <form class="form-group" action="{{route('store')}}">
+                @csrf
+                <input class="form-control" type="text" name="name">
+                <input class="form-control" type="text" name="price">
+                <input class="btn btn-primary" type="submit" name="name">
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</head>
-<body>
+@extends('layout.app')
+
+@section('name')    
+<div><a class="btn btn-success" href="{{route('create')}}">add</a></div>
     <div class="container">
         <table class="table table-hover">
             <thead >
@@ -39,5 +30,4 @@
             </tbody>
         </table>
     </div>
-</body>
-</html>
+@endsection
